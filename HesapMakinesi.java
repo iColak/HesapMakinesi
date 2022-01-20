4cd505b9f37cc232b4e169d6aa198ab03395c9ac
iskenderColak <60299811+iColak@users.noreply.github.com>
2022-01-20 16:04:09 +0300

    Add files via upload

---
+++ HesapMakinesi.java
@@ -0,0 +1,37 @@
+package kosulluIfadeler;
+
+import java.util.Scanner;
+
+public class HesapMakinesi {
+    public static void main(String[] args) {
+        int n1, n2, select;
+        Scanner input = new Scanner(System.in);
+        System.out.print("İlk sayıyı giriniz: ");
+        n1 = input.nextInt();
+        System.out.print("İkinci sayıyı giriniz: ");
+        n2 = input.nextInt();
+        System.out.println("1- Toplama\n2- Çıkarma\n3- Çarpma\n4- Bölme");
+        System.out.print("Seçiminizi Yapınız: ");
+        select = input.nextInt();
+        switch (select) {
+            case 1:
+                System.out.println("Sonuç : " + (n1 + n2));
+                break;
+            case 2:
+                System.out.println("Sonuç : " + (n1 - n2));
+                break;
+            case 3:
+                System.out.println("Sonuç : " + (n1 * n2));
+                break;
+            case 4:
+                if (n2 != 0) {
+                    System.out.println("Sonuç : " + (n1 / n2));
+                } else {
+                    System.out.println("Bir sayı 0'a bölünemez.");
+                }
+                break;
+            default:
+                System.out.println("Yanlış seçim yaptınız, tekrar deneyiniz");
+        }
+    }
+}
